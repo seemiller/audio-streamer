7ba89821f842fcf935a4dad228f3d7e0901852e2
Nick Seemiller <nick.seemiller@betterup.co>
2024-09-10 17:10:56 -0600

    use the text query param

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -3,15 +3,26 @@ require 'net/https'
 
 class StreamController < ApplicationController
   def tts
-    response.headers['Content-Type'] = 'audio/mpeg'
-    text = params[:text]
-
     begin
-      short = 'short Hello, how are you doing today?'
-      med =  "medium medium Well, I would do that, and we’re sitting down. You know, I was somebody — we had, Senator Marco Rubio, and my daughter Ivanka, was so impactful on that issue. It’s a very important issue. But I think when you talk about the kind of numbers that I’m talking about — that, because look, child care is child care, couldn’t — you know, there’s something — you have to have it in this country. You have to have it. But when you talk about those numbers, compared to the kind of numbers that I’m talking about by taxing foreign nations at levels that they’re not used to. But they’ll get used to it very quickly. And it’s not going to stop them from doing business with us."
-      long =  "long long long Well, I would do that, and we’re sitting down. You know, I was somebody — we had, Senator Marco Rubio, and my daughter Ivanka, was so impactful on that issue. It’s a very important issue. But I think when you talk about the kind of numbers that I’m talking about — that, because look, child care is child care, couldn’t — you know, there’s something — you have to have it in this country. You have to have it. But when you talk about those numbers, compared to the kind of numbers that I’m talking about by taxing foreign nations at levels that they’re not used to. But they’ll get used to it very quickly. And it’s not going to stop them from doing business with us. But they’ll have a very substantial tax when they send product into our country. Those numbers are so much bigger than any numbers that we’re talking about, including child care, that it’s going to take care. We’re going to have — I look forward to having no deficits within a fairly short period of time, coupled with the reductions that I told you about on waste and fraud and all of the other things that are going on in our country. Because I have to stay with child care. I want to stay with child care. But those numbers are small relative to the kind of economic numbers that I’m talking about, including growth, but growth also headed up by what the plan is that I just — that I just told you about. We’re going to be taking in trillions of dollars. And as much as child care is talked about as being expensive, it’s, relatively speaking, not very expensive compared to the kind of numbers will be taking in. We’re going to make this into an incredible country that can afford to take care of its people. And then we’ll worry about the rest of the world. Let’s help other people. But we’re going to take care of our country first. This is about America first. It’s about make America great again. We have to do it because right now, we’re a failing nation. So we’ll take care of it. Thank you. Very good question. Thank you."
+      quick = 'Hello, how are you doing today?'
+      text_params = params[:text]
+      text = if text_params.present?
+                case text_params
+                when 'q'
+                  quick
+                when 'm'
+                  "peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots peas and carrots"
+                when 'l'
+                  "Well, I would do that, and we’re sitting down. You know, I was somebody — we had, Senator Marco Rubio, and my daughter Ivanka, was so impactful on that issue. It’s a very important issue. But I think when you talk about the kind of numbers that I’m talking about — that, because look, child care is child care, couldn’t — you know, there’s something — you have to have it in this country. You have to have it. But when you talk about those numbers, compared to the kind of numbers that I’m talking about by taxing foreign nations at levels that they’re not used to. But they’ll get used to it very quickly. And it’s not going to stop them from doing business with us. But they’ll have a very substantial tax when they send product into our country. Those numbers are so much bigger than any numbers that we’re talking about, including child care, that it’s going to take care. We’re going to have — I look forward to having no deficits within a fairly short period of time, coupled with the reductions that I told you about on waste and fraud and all of the other things that are going on in our country. Because I have to stay with child care. I want to stay with child care. But those numbers are small relative to the kind of economic numbers that I’m talking about, including growth, but growth also headed up by what the plan is that I just — that I just told you about. We’re going to be taking in trillions of dollars. And as much as child care is talked about as being expensive, it’s, relatively speaking, not very expensive compared to the kind of numbers will be taking in. We’re going to make this into an incredible country that can afford to take care of its people. And then we’ll worry about the rest of the world. Let’s help other people. But we’re going to take care of our country first. This is about America first. It’s about make America great again. We have to do it because right now, we’re a failing nation. So we’ll take care of it. Thank you. Very good question. Thank you."
+                else
+                  text_params
+                end
+       else
+         quick
+       end
+
       sse_stream_handler(raw: true) do |stream|
-        stream_audio(text: med, response_stream: stream)
+        stream_audio(text: text, response_stream: stream)
       end
     rescue => e
       logger.error("Error occurred: #{e.message}")
